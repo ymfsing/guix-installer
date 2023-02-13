78105358e7adfd745159e0886c29c4f28bb97543
ymfsing <ymfsing@outlook.com>
2023-02-13 15:13:21 +0800

    add nongnu substitute

--- guix/installer.scm
+++ guix/installer.scm
@@ -30,30 +30,40 @@
   #:use-module (gnu packages mtools)
   #:use-module (gnu packages package-management)
   #:use-module (nongnu packages linux)
+  #:use-module (gnu)
   #:use-module (guix)
   #:export (installation-os-nonfree))
-
 (define installation-os-nonfree
   (operating-system
-   (inherit installation-os)
-   (kernel linux)
-   (firmware (list linux-firmware))
+    (inherit installation-os)
+    (kernel linux)
+    (firmware (list linux-firmware))
 
-   ;; Add the 'net.ifnames' argument to prevent network interfaces
-   ;; from having really long names.  This can cause an issue with
-   ;; wpa_supplicant when you try to connect to a wifi network.
-   (kernel-arguments '("quiet" "modprobe.blacklist=radeon" "net.ifnames=0"))
+    ;; Add the 'net.ifnames' argument to prevent network interfaces
+    ;; from having really long names.  This can cause an issue with
+    ;; wpa_supplicant when you try to connect to a wifi network.
+    (kernel-arguments '("quiet" "modprobe.blacklist=radeon" "net.ifnames=0"))
 
-   (services
-    (cons*
-     ;; Include the channel file so that it can be used during installation
-     (simple-service 'channel-file etc-service-type
-                     (list `("channels.scm" ,(local-file "channels.scm"))))
-     (operating-system-user-services installation-os)))
+    (services
+     (cons*
+      ;; Include the channel file so that it can be used during installation
+      (simple-service 'channel-file etc-service-type
+                      (list `("channels.scm" ,(local-file "channels.scm"))
+                            `("key.pub" ,(local-file "key.pub"))))
+      
+      (modify-services (operating-system-user-services installation-os)
+             (guix-service-type config => (guix-configuration
+               (inherit config)
+               (substitute-urls
+                (append (list "https://substitutes.nonguix.org")
+                  %default-substitute-urls))
+               (authorized-keys
+                (append (list (local-file "key.pub"))
+                  %default-authorized-guix-keys)))))))
 
-   ;; Add some extra packages useful for the installation process
-   (packages
-    (append (list git curl vim emacs-no-x-toolkit)
-            (operating-system-packages installation-os)))))
+    ;; Add some extra packages useful for the installation process
+    (packages
+     (append (list git curl stow vim emacs-no-x-toolkit)
+             (operating-system-packages installation-os)))))
 
 installation-os-nonfree
